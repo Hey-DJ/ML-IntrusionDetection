b916abbce89c5a10b621092fa4f6ce6459b5e012
Hey-DJ <70017625+Hey-DJ@users.noreply.github.com>
2024-02-04 14:28:33 +1100

    Completed submission

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -43,9 +43,113 @@ def test_train_comparison(y_train, pred_train, y_test, pred_test):
     display(df.style.format('{:,}').background_gradient())
 
 
+
+def plot_distribution(X):
+    """Plots the distribution of X using these 3 types of plots:
+            1) Distribution Plot
+            2) Box Plot
+            3) Probability Plot
+       X needs to be a Pandas Series
+    """
+    print('Feature is of type: ', X.dtype)
+    print('5 sample values from this feature:', X.unique()[0:5])
+    f, ax = plt.subplots(nrows=1, ncols=3, figsize=(18, 4));
+    sns.histplot(X, ax=ax[0], kde=True, stat="density", kde_kws=dict(cut=3))
+    #sns.distplot(X, ax=ax[0]);
+    sns.boxplot(X, ax=ax[1]);
+    stats.probplot(X, plot=ax[2]);
+    plt.show();
+
+
+
+def get_feature_names(column_transformer):
+    """Get feature names from all transformers.
+    Returns
+    -------
+    feature_names : list of strings
+        Names of the features produced by transform.
+    """
+    # Remove the internal helper function
+    #check_is_fitted(column_transformer)
+    
+    # Turn loopkup into function for better handling with pipeline later
+    def get_names(trans):
+        # >> Original get_feature_names() method
+        if trans == 'drop' or (
+                hasattr(column, '__len__') and not len(column)):
+            return []
+        if trans == 'passthrough':
+            if hasattr(column_transformer, '_df_columns'):
+                if ((not isinstance(column, slice))
+                        and all(isinstance(col, str) for col in column)):
+                    return column
+                else:
+                    return column_transformer._df_columns[column]
+            else:
+                indices = np.arange(column_transformer._n_features)
+                return ['x%d' % i for i in indices[column]]
+        if not hasattr(trans, 'get_feature_names'):
+        # >>> Change: Return input column names if no method avaiable
+            # Turn error into a warning
+            warnings.warn("Transformer %s (type %s) does not "
+                                 "provide get_feature_names. "
+                                 "Will return input column names if available"
+                                 % (str(name), type(trans).__name__))
+            # For transformers without a get_features_names method, use the input
+            # names to the column transformer
+            if column is None:
+                return []
+            else:
+                return [name + "__" + f for f in column]
+
+        return [name + "__" + f for f in trans.get_feature_names()]
+    
+    ### Start of processing
+    feature_names = []
+    
+    # Allow transformers to be pipelines. Pipeline steps are named differently, so preprocessing is needed
+    if type(column_transformer) == sklearn.pipeline.Pipeline:
+        l_transformers = [(name, trans, None, None) for step, name, trans in column_transformer._iter()]
+    else:
+        # For column transformers, follow the original method
+        l_transformers = list(column_transformer._iter(fitted=True))
+    
+    
+    for name, trans, column, _ in l_transformers: 
+        if type(trans) == sklearn.pipeline.Pipeline:
+            # Recursive call on pipeline
+            _names = get_feature_names(trans)
+            # if pipeline has no transformer that returns names
+            if len(_names)==0:
+                _names = [name + "__" + f for f in column]
+            feature_names.extend(_names)
+        else:
+            feature_names.extend(get_names(trans))
+    
+    return feature_names
+
+
+def beep():
+    import winsound
+
+    winsound.Beep(2093, 180)
+    winsound.Beep(2093, 180)
+    winsound.Beep(3136, 180)
+    winsound.Beep(3136, 180)
+    winsound.Beep(3520, 180)
+    winsound.Beep(3520, 180)
+    winsound.Beep(3136, 720)
+    winsound.Beep(2794, 180)
+    winsound.Beep(2794, 180)
+    winsound.Beep(2637, 180)
+    winsound.Beep(2637, 180)
+    winsound.Beep(2349, 180)
+    winsound.Beep(2349, 180)
+    winsound.Beep(2093, 720)
+
+
 def classification_report_IDS(y_true, y_prediction):
-    '''My version of the sklearn's classification_report function. Instead of the F1 score, it returns the F2 score 
-    and Matthews Correlation Coefficient (MCC)'''
+    '''My version of the sklearn's classification_report function. Instead of the F1 score, it returns the F2 score'''
     from sklearn.metrics import precision_recall_fscore_support
     from sklearn.metrics import matthews_corrcoef
     from sklearn.metrics import fbeta_score
